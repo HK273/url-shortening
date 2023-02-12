18c2ab68e58126ac02d62ae34156d82177ffb080
Hector Kurtyanek <hkurtyanek@adaptavist.com>
2023-02-12 22:17:26 +0000

    update custom props

--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -11,15 +11,15 @@ function Navtwo() {
   return (
     <header>
       <nav className="menu navbar">
-        <img className="logo" src="images/logo.svg" alt="logo" />
+        <img className="logo_one" src="images/logo.svg" alt="logo" />
         <ul className={`nav-menu ${isOpen ? "active" : ""}`}>
           <img className="logo" src="images/logo.svg" alt="logo" />
-          <li className="item">About</li>
-          <li className="item">Services</li>
-          <li className="item">Projects</li>
-          <span className="space"></span>
+          <li className="item">Features</li>
+          <li className="item">Pricing</li>
+          <li className="item">Resources</li>
+          <li className="item">Login</li>
           <li className="nav-item item">
-            <button className="buttons">CONTACT</button>
+            <button className="btn">Sign Up</button>
           </li>
         </ul>
         <div
--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Navbar from "./components/layouts/Navbar";
-import Hero from "./components/layouts/Hero";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Navbar />
-    <Hero />
   </React.StrictMode>
 );
