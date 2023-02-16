219bad073022ac0bcf8860b5172201c935794a85
Hector Kurtyanek <hkurtyanek@adaptavist.com>
2023-02-16 16:39:34 +0000

    update section component

--- src/components/layouts/SectionTwo.js
+++ src/components/layouts/SectionTwo.js
@@ -1,7 +1,11 @@
 import "./SectionTwo.css";
 
 function SectionTwo() {
-  return <section className="section-two"></section>;
+  return (
+    <section className="section-two">
+      <div className="Test">Hello</div>
+    </section>
+  );
 }
 
 export default SectionTwo;
